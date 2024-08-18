620467fc6bc1d99b66aae085c947257ad9901e53
sansidsac <kssanju04@gmail.com>
2024-08-18 23:45:49 +0530

    CORS Policy handling added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
 import bookRoute from "./routes/bookRoutes.js";
+import cors from 'cors';
 
 const app=express();
 
@@ -9,10 +10,22 @@ const app=express();
 
 app.use(express.json());
 
+//Middleware for handling CORS Policy
+//Option 1: Allow all origins with default of cors(*)
+//app.use(cors());
+//Option 2: Allow custom origins
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type']
+}))
+
 app.get('/',async(request,response)=>{
     console.log(request);
     return response.status(234).send('Welcome to MERN stack')
 })
+
+// Using routes
 
 app.use('/books', bookRoute)
 
