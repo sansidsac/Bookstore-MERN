0d3ee0c2016fc1d9ec47fb52faabab3cc581c849
sansidsac <kssanju04@gmail.com>
2024-08-21 19:30:14 +0530

    Implemented React Router Setup for SPA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import CreateBook from './components/CreateBook'
+import DeleteBook from './components/DeleteBook'
+import EditBook from './components/EditBook'
+import ShowBook from './components/ShowBook'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div className='bg-blue-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/books/create' element={<CreateBook/>}></Route>
+      <Route path='/books/details/:id' element={<ShowBook/>}></Route>
+      <Route path='/books/edit/:id' element={<EditBook/>}></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}></Route>
+    </Routes>
   )
 }
 
