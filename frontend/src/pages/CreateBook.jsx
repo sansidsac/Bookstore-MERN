46c613f16d7a453b5fafe534786d509cc04270c2
sansidsac <kssanju04@gmail.com>
2024-08-25 18:28:51 +0530

    Added notification feature using SnackbarProvider and removed unnecessary codes and files

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { useSnackbar } from 'notistack'
 
 const CreateBook = () => {
   const[title,setTitle]=useState('')
@@ -10,7 +11,7 @@ const CreateBook = () => {
   const[publishYear,setPublishYear]=useState('')
   const[loading,setLoading]=useState(false)
   const navigate=useNavigate()
-
+  const {enqueueSnackbar}=useSnackbar()
   const handleSaveBook=()=>{
     const data={
       title,
@@ -21,11 +22,12 @@ const CreateBook = () => {
     axios.post('http://localhost:5555/books',data)
     .then(()=>{
       setLoading(false);
+      enqueueSnackbar('Book Created Successfully',{variant:'success'})
       navigate('/')
     })
     .catch((error)=>{
       setLoading(false);
-      alert('An error happend, please check console');
+      enqueueSnackbar('Error',{variant:'error'})
       console.log(error);
     })
   }
